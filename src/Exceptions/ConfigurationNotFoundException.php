b86ad050acd6ada88663dd9f038b9dec4ff26941
Brian Faust <brian@cline.sh>
2025-11-19 08:38:24 +0200

    first commit
    Signed-off-by: Brian Faust <brian@cline.sh>

---
+++ src/Exceptions/ConfigurationNotFoundException.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) Brian Faust
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cline\Manager\Exceptions;
+
+use InvalidArgumentException;
+
+use function sprintf;
+
+/**
+ * @author Brian Faust <brian@cline.sh>
+ */
+final class ConfigurationNotFoundException extends InvalidArgumentException
+{
+    public static function forConfiguration(string $description, string $name): self
+    {
+        return new self(sprintf('%s [%s] not configured.', $description, $name));
+    }
+}
---
+++ src/Exceptions/ExtensionMustReturnObjectException.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) Brian Faust
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cline\Manager\Exceptions;
+
+use InvalidArgumentException;
+
+use function sprintf;
+
+/**
+ * @author Brian Faust <brian@cline.sh>
+ */
+final class ExtensionMustReturnObjectException extends InvalidArgumentException
+{
+    public static function forExtension(string $name): self
+    {
+        return new self(sprintf('Extension for [%s] must return an object', $name));
+    }
+
+    public static function forDriver(string $driver): self
+    {
+        return new self(sprintf('Extension for driver [%s] must return an object', $driver));
+    }
+}
